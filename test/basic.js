3fb63f3a48743217a151c2d9966b050fb06ed78b
Cam Pedersen <diffference@gmail.com>
2012-06-07 11:34:41 -0400

    lay out lib structure

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,7 @@
-var TipClient = require('../');
-var tips = new TipClient({
+var TipsClient = require('../');
+var tips = new TipsClient({
   key: 'a71011cb4c8f4792ab339b64c4a97799',
-  secret: '897ec47bda7147afa53d4fe926330532'
+  secret: '897ec47bda7147afa53d4fe926330532',
+  debug: true
 });
 
